e5a6a89b1825ef3debd2d37b4ffe4814b6a91461
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2023-10-06 11:52:34 +0530

    commit

---
+++ NUnitSelenium/bin/Release/NUnitSeleniumTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace NUnitSelenium
+{
+    [TestFixture("chrome", "60", "Windows 10")]
+    [TestFixture("internet explorer", "11", "Windows 7")]
+    [TestFixture("firefox", "60", "Windows 7")]
+    [TestFixture("chrome", "71", "Windows 7")]
+    [TestFixture("internet explorer", "11", "Windows 10")]
+    [TestFixture("firefox", "58", "Windows 7")]
+    [TestFixture("chrome", "67", "Windows 7")]
+    [TestFixture("internet explorer", "10", "Windows 7")]
+    [TestFixture("firefox", "55", "Windows 7")]
+    [Parallelizable(ParallelScope.Children)]
+    public class NUnitSeleniumSample
+    {
+        public static string LT_USERNAME ="shubhamr";
+        public static string LT_ACCESS_KEY = "dl8Y8as59i1YyGZZUeLF897aCFvIDmaKkUU1e6RgBmlgMLIIhh";
+        public static string seleniumUri = "https://hub.lambdatest.com:443/wd/hub";
+
+
+        ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+        private String browser;
+        private String version;
+        private String os;
+
+        public NUnitSeleniumSample(String browser, String version, String os)
+        {
+            this.browser = browser;
+            this.version = version;
+            this.os = os;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(CapabilityType.BrowserName, browser);
+            capabilities.SetCapability(CapabilityType.Version, version);
+            capabilities.SetCapability(CapabilityType.Platform, os);
+          
+            capabilities.SetCapability("user", "shubhamr");
+            capabilities.SetCapability("accessKey", "dl8Y8as59i1YyGZZUeLF897aCFvIDmaKkUU1e6RgBmlgMLIIhh");
+
+            capabilities.SetCapability("name",
+            String.Format("{0}:{1}",
+            TestContext.CurrentContext.Test.ClassName,
+            TestContext.CurrentContext.Test.MethodName));
+            driver.Value = new RemoteWebDriver(new Uri(seleniumUri), capabilities, TimeSpan.FromSeconds(600));
+            Console.Out.WriteLine(driver);
+        }
+
+        [Test]
+        [Category("SeleniumPlayGround")]
+       public void Todotest()
+        {
+            {
+                Console.WriteLine("Navigating to todos app.");
+                driver.Value.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
+
+                driver.Value.FindElement(By.Name("li4")).Click();
+                Console.WriteLine("Clicking Checkbox");
+                driver.Value.FindElement(By.Name("li5")).Click();
+
+
+                // If both clicks worked, then te following List should have length 2
+                IList<IWebElement> elems = driver.Value.FindElements(By.ClassName("done-true"));
+                // so we'll assert that this is correct.
+                Assert.AreEqual(2, elems.Count);
+
+                Console.WriteLine("Entering Text");
+                driver.Value.FindElement(By.Id("sampletodotext")).SendKeys("Yey, Let's add it to list");
+                driver.Value.FindElement(By.Id("addbutton")).Click();
+
+
+                // lets also assert that the new todo we added is in the list
+                string spanText = driver.Value.FindElement(By.XPath("/html/body/div/div/div/ul/li[6]/span")).Text;
+                Assert.AreEqual("Yey, Let's add it to list", spanText);
+
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            bool passed = TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Passed;
+            try
+            {
+                // Logs the result to LambdaTest
+                ((IJavaScriptExecutor)driver.Value).ExecuteScript("lambda-status=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                
+                // Terminates the remote webdriver session
+                driver.Value.Quit();
+            }
+        }
+    }
+}
